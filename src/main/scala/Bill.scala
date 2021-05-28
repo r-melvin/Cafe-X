a99a3de44b9b8cab9f80d6c2a2fc40ad919f6ae7
Richard Melvin <40235254+r-melvin@users.noreply.github.com>
2021-05-28 12:03:04 +0100

    Updates

--- src/main/scala/Bill.scala
+++ src/main/scala/Bill.scala
@@ -5,26 +5,21 @@ import scala.math.BigDecimal.RoundingMode.HALF_EVEN
 
 object Bill {
 
-  private def format(bill: BigDecimal): String = getCurrencyInstance.format(bill.setScale(2, HALF_EVEN))
-
   def getTotalCost(order: List[MenuItem], optLoyaltyCard: Option[LoyaltyCard] = None): String = {
 
-    val getServiceCharge: ServiceCharge = order.map(_.serviceCharge).maxByOption(_.value).getOrElse(NoServiceCharge)
-    val cost: BigDecimal = order.map(_.price).sum
-
     val preServiceChargeCost: BigDecimal = optLoyaltyCard match {
-      case Some(loyalty) if !order.exists(_.itemType == Premium) => cost * loyalty.percentageDecrease
-      case _ => cost
+      case Some(loyalty) if !order.exists(_.itemType == Premium) => order.map(_.price).sum * loyalty.percentageDecrease
+      case _ => order.map(_.price).sum
     }
 
-    val serviceCharge: BigDecimal = getServiceCharge match {
-      case TenPercent => preServiceChargeCost * TenPercent.value
-      case TwentyPercent => (preServiceChargeCost * TwentyPercent.value).min(MaxServiceCharge.value)
-      case TwentyFivePercent => (preServiceChargeCost * TwentyFivePercent.value).min(MaxPremiumServiceCharge.value)
+    val serviceCharge: BigDecimal = order.map(_.serviceCharge).maxByOption(_.value) match {
+      case Some(TenPercent) => preServiceChargeCost * TenPercent.value
+      case Some(TwentyPercent) => (preServiceChargeCost * TwentyPercent.value).min(MaxServiceCharge.value)
+      case Some(TwentyFivePercent) => (preServiceChargeCost * TwentyFivePercent.value).min(MaxPremiumServiceCharge.value)
       case _ => 0
     }
 
-    format(preServiceChargeCost + serviceCharge)
+    getCurrencyInstance.format((preServiceChargeCost + serviceCharge).setScale(2, HALF_EVEN))
   }
 
 }
